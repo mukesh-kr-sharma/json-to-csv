589196b7f57ae8903c295453d161c2acfccb9186
Sharma, Mukesh <musharma@levi.com>
2024-03-29 20:44:51 +0530

    incorporated uvicorn in main file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import csv
 from io import StringIO
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -70,4 +71,12 @@ def json_to_csv(json_file: UploadFile):
         media_type='text/csv',
         headers={
             "Content-Disposition": f"attachment;filename={json_file.filename.rsplit('.')[0]}.csv"}
+    )
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app", 
+        # host="0.0.0.0", 
+        # port=5000, 
+        log_level="info"
     )
